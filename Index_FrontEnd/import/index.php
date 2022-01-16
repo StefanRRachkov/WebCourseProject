7151663f20e3d33b2a0302483ac165fb306ecf46
Emanuil Gospodinov <gospodinove@gmail.com>
2022-01-17 00:37:38 +0200

    Drag and drop support on import page

--- Index_FrontEnd/import/index.php
+++ Index_FrontEnd/import/index.php
@@ -52,8 +52,8 @@
 
     <section class="upload" <? echo $hidden ?>>
         <div class="upload-form-container">
-            <div class="upload-drag-and-drop-pad">
-                <div class="upload-drag-and-drop-pad-label">Drop file here</div>
+            <div id="drop-pad" class="drag-and-drop-pad">
+                <div class="drag-and-drop-pad-label">Drop file here</div>
             </div>
 
             <form id="file-form" action="../../Index_BackEnd" method="POST">
@@ -79,20 +79,42 @@
 </body>
 
 <script>
-    document.getElementById("upload-file-button").addEventListener("click", (event) => {
+    const dropPad = document.getElementById('drop-pad');
+    const uploadFileButton = document.getElementById("upload-file-button");
+    const fileInput = document.getElementById("file-input");
+    const submitButton = document.getElementById('file-form-submit-button');
+
+    dropPad.addEventListener('drop', (event) => {
+        event.preventDefault();
+
+        if (event.dataTransfer.files.length) {
+            fileInput.files = event.dataTransfer.files;
+        }
+
+        dropPad.classList.remove('drag-and-drop-pad-over')
+    });
+
+    dropPad.addEventListener('dragover', (event) => {
+        event.preventDefault();
+        dropPad.classList.add('drag-and-drop-pad-over');
+    });
+
+    ["dragleave", "dragend"].forEach((type) => {
+        dropPad.addEventListener(type, (event) => {
+            dropPad.classList.remove("drag-and-drop-pad-over");
+        });
+    });
+
+    uploadFileButton.addEventListener("click", (event) => {
         event.preventDefault();
 
         document.getElementById('file-input').click();
     });
 
-    document.getElementById("file-input").addEventListener('change', (event) => {
-        const submitButton = document.getElementById('file-form-submit-button');
-        const fileInput = document.getElementById('file-input');
+    fileInput.addEventListener('change', (event) => {
         const fileName = fileInput.files[0].name;
 
-        const fileUploadButton = document.getElementById("upload-file-button");
-
-        fileUploadButton.innerHTML = fileName;
+        uploadFileButton.innerHTML = fileName;
         
         submitButton.disabled = false;
     });
