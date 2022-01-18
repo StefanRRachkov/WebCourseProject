82f96cbcdcb2ef88162d38997fafb0c00969ed28
LuskaZakuska <94144098+LuskaZakuska@users.noreply.github.com>
2022-01-19 00:32:34 +0200

    Add login and register functions in DBConnection

--- Index_BackEnd/DBConnection.php
+++ Index_BackEnd/DBConnection.php
@@ -53,6 +53,75 @@ class DBConnection {
       return $e->getMessage();
     }
   }
+
+  public function login($email, $password){
+    $hashed_password = sha1($password);
+  
+    try {
+      $sql = 'SELECT * FROM Users WHERE email=? and password = ?';
+      $stmt = $this->$connection->prepare($sql);
+      $result = $stmt->execute(array($email, $hashed_password));
+  
+      if ($result && $stmt->rowCount() == 1) {
+        $_SESSION['user'] = $email;
+      }
+      else{
+        $_SESSION['loginError'] = "Wrong email or password";
+      }
+    }
+    catch(Exception $e) {
+      $_SESSION['loginError'] = "No connection with DB";
+      //var_dump($e->getMessage());
+    }
+  }
+
+  public function register($email, $password, $pass2) {
+    if(strlen($password) < 6){
+      $_SESSION['regError'] = "Password is too short";
+      $this->finishRegister();
+    }
+    
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $_SESSION['regError'] = "Incorrect Email Syntax";
+      $this->finishRegister();
+    }
+    
+    if($password != $pass2){
+      $_SESSION['regError'] = "Passwords are different";
+      $this->finishRegister();
+    }
+    
+    $hashed_password = sha1($password);
+    
+    //---------insert record in DB--------------------------------------
+    try {
+      $conn = new PDO('mysql:host=localhost;dbname=web_take_a_ref', 'root', '');	
+      $sql = 'INSERT INTO Users (email, password) VALUES (?, ?)';
+      $stmt = $conn->prepare($sql);
+      $result = $stmt->execute(array($email, $hashed_password));
+    
+      if ($result && $stmt->rowCount() == 1) {
+        $_SESSION['user'] = $email;
+      }
+      $this->finishRegister();
+    }
+    catch(Exception $e) {
+      $error = $stmt->errorInfo();
+        if ($error[1] == 1062) {
+        $_SESSION['regError'] = 'Email is already registered';
+        } 
+      else{
+        $_SESSION['regError'] = "No connection with DB<br>".$e->getMessage();
+      }
+      //var_dump($e->getMessage());
+      $this->finishRegister();
+    }
+  }
+
+  public function finishRegister(){
+    header('Location: index.php#login');
+    exit();
+  }
 }
 
 ?>
