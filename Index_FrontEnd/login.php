14fae0b8fde830af052bbee28dda00dfc25c6c8e
LuskaZakuska <94144098+LuskaZakuska@users.noreply.github.com>
2022-01-19 00:33:38 +0200

    Use DBConnection login method

--- Index_FrontEnd/login.php
+++ Index_FrontEnd/login.php
@@ -1,33 +1,13 @@
 <?php
 
+	include '../Index_BackEnd/DBConnection.php';
+
 	session_start();
 
 	$email = $_POST['email'];
 	$password = $_POST['pass'];
-	$hashed_password = sha1($password);
-
-	//check if login data is valid
-	try {
-		$conn = new PDO('mysql:host=localhost;dbname=web_take_a_ref', 'root', '');	
-		$sql = 'SELECT * FROM Users WHERE email=? and password = ?';
-		$stmt = $conn->prepare($sql);
-		$result = $stmt->execute(array($email, $hashed_password));
-
-		if ($result && $stmt->rowCount() == 1) {
-			$_SESSION['user'] = $email;
-		}
-		else{
-			$_SESSION['loginError'] = "Wrong email or password";
-		}
-	}
-	catch(Exception $e) {
-		$_SESSION['loginError'] = "No connection with DB";
-		//var_dump($e->getMessage());
-	}
+	$conn = DBConnection::sharedInstance()->login($email, $password);
 
 	header('Location: index.php#login');
-
-	
-
 
 ?>
