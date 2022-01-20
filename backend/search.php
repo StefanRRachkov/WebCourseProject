505bcb29afd4f7b290a6355126451e6bf8834948
Stefan Rachkov <astrex17@abv.bg>
2022-01-20 21:27:35 +0200

    Fix bug with search when user is not logged in

--- backend/search.php
+++ backend/search.php
@@ -2,12 +2,13 @@
     include 'DBConnection.php';
 
     $condition = '';
-    if(!empty($_POST['search']))
-    {
-        $condition = $_POST['search'];
-    }
-
     $userId = $_SESSION['user'];
 
-    $result = DBConnection::sharedInstance()->getReferatsWithConditions($userId, $condition);
+    if($userId != null){
+        if(!empty($_POST['search']))
+        {
+            $condition = $_POST['search'];
+        }
+        $result = DBConnection::sharedInstance()->getReferatsWithConditions($userId, $condition);
+    }
 ?>
